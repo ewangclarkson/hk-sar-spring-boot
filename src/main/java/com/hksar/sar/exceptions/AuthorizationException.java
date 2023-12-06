7cb9b10992288a43c4344bbe4c32d39464a36738
ewangclarkson <ewangclarks@gmail.com>
2023-12-06 11:22:02 +0100

    fix bug

--- src/main/java/com/hksar/sar/exceptions/AuthorizationException.java
+++ src/main/java/com/hksar/sar/exceptions/AuthorizationException.java
@@ -1,7 +1,9 @@
 package com.hksar.sar.exceptions;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class AuthorizationException extends RuntimeException {
     public AuthorizationException(String message) {
